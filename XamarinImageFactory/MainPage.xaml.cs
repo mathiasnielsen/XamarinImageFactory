3e875c344b28323dc711aa3e8c6ff45a7b47bec5
Mathias <Mathias@DESKTOP-9FDQVBJ>
2017-03-07 14:45:44 +0100

    Initial

--- XamarinImageFactory/MainPage.xaml.cs
+++ XamarinImageFactory/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,53 @@ namespace XamarinImageFactory
         public MainPage()
         {
             this.InitializeComponent();
+
+            PrepareUIElements();
+        }
+
+        private void PrepareUIElements()
+        {
+            PickButton.Click += OnPickButtonClicked;
+        }
+
+        private async void OnPickButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+            }
+            else
+            {
+                // cancelled
+            }
+        }
+
+        private async Task SaveFilesAsync(List<StorageFile> files)
+        {
+            var folder = CreateFolderForImagesAsync();
+
+            SaveFileAsync(folder, files)
+        }
+
+        private async Task SaveFileAsync(StorageFolder folder, string filename)
+        {
+            await folder.CreateFileAsync(filename);
+        }
+
+        private async Task<StorageFolder> CreateFolderForImagesAsync()
+        {
+            var folderName = string.IsNullOrWhiteSpace(FolderNameTextBox.Text) ? Guid.NewGuid().ToString() : FolderNameTextBox.Text;
+            var folder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+
+            return folder;
         }
     }
 }
